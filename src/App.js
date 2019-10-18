304639e6c61344167cc6a635a7b2e5fc24036dbe
meissadia <meissadia@gmail.com>
2019-10-18 09:40:54 -0600

    Setup initial project structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import { LandingPage } from "./LandingPage";
+import { MonthSelection } from "./MonthSelection";
+import { CalendarView } from "./CalendarView";
+import { StrategyView } from "./StrategyView";
+import { TestNav } from "./TestNav";
+
+import "./styles/App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <TestNav />
+      <Switch>
+        <Route path="/" exact component={LandingPage} />
+        <Route path="/month" component={MonthSelection} />
+        <Route path="/calendar" component={CalendarView} />
+        <Route path="/strategies" component={StrategyView} />
+      </Switch>
     </div>
   );
 }
---
+++ src/LandingPage.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const LandingPage = () => <p>Landing Page</p>;
