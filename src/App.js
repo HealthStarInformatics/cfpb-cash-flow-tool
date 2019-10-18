58cd4ae28ec611f268ed3ad4c470d86577ea4301
meissadia <meissadia@gmail.com>
2019-10-18 11:23:16 -0600

    Add Context. Fix misplaced files. Add .jsx to Component file names.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { LandingPage } from "./LandingPage";
-import { MonthSelection } from "./MonthSelection";
-import { CalendarView } from "./CalendarView";
-import { StrategyView } from "./StrategyView";
-import { TestNav } from "./TestNav";
-
+import { LandingPage } from "./components/LandingPage";
+import { MonthSelection } from "./components/MonthSelection";
+import { CalendarView } from "./components/CalendarView";
+import { StrategyView } from "./components/StrategyView";
+import { TestNav } from "./components/TestNav";
 import "./styles/App.scss";
 
-function App() {
-  return (
-    <div className="app">
-      <TestNav />
-      <Switch>
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/month" component={MonthSelection} />
-        <Route path="/calendar" component={CalendarView} />
-        <Route path="/strategies" component={StrategyView} />
-      </Switch>
-    </div>
-  );
+const Context = React.createContext();
+
+class App extends React.Component {
+  state = {
+    incomes: {},
+    expenses: {}
+  };
+
+  render = () => {
+    const contextObject = {
+      ...this.state,
+      setState: this.setState.bind(this)
+    };
+
+    return (
+      <Context.Provider value={contextObject}>
+        <TestNav />
+        <Switch>
+          <Route path="/" exact component={LandingPage} />
+          <Route path="/month" component={MonthSelection} />
+          <Route path="/calendar" component={CalendarView} />
+          <Route path="/strategies" component={StrategyView} />
+        </Switch>
+      </Context.Provider>
+    );
+  };
 }
 
 export default App;
