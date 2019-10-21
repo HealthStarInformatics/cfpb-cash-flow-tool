5fed13030cd53ab8a70bac1cd04e6945b793ec20
meissadia <meissadia@gmail.com>
2019-10-21 13:25:22 -0600

    Add MonthSelect. Save selectedMonth in AppContext. Unify <button> styles.

---
+++ src/components/MonthSelectView.jsx
@@ -0,0 +1,39 @@
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import Select from "react-select";
+import { AppContext } from "../App";
+import headerImg from "../assets/month-select-header.png";
+import { generateMonthOptions } from "../services/dateServices";
+import "../styles/View.scss";
+import { Divider } from "./shared/Divider";
+import { ViewHeader } from "./ViewHeader";
+
+export const MonthSelectView = () => {
+  const { selectedMonth, setState } = useContext(AppContext);
+
+  return (
+    <div className="view month">
+      <ViewHeader
+        image={headerImg}
+        imgAltText="empty calendar"
+        title="Start by selecting a month."
+      />
+      <Select
+        value={selectedMonth}
+        onChange={opt => setState({ selectedMonth: opt })}
+        options={generateMonthOptions()}
+        placeholder="Select a month"
+      />
+      <div className="actions">
+        <Link to="/calendar">
+          <button className="primary">Next: Customize your calendar</button>
+        </Link>
+      </div>
+      <Divider color="dark" />
+
+      {/* TODO */}
+      {/* <SiteFooter /> */}
+      {/* <PrivacyNotification /> */}
+    </div>
+  );
+};
