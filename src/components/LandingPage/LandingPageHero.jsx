8fa4e7990085ae44a959db3e40a8e5503a00ba5b
Missy Maloney <mmaloney@healthstarInfo.com>
2019-11-08 09:57:44 -0500

    completed save to/retreive from localStorage; service worker; removed reference to mock data

--- src/components/LandingPage/LandingPageHero.jsx
+++ src/components/LandingPage/LandingPageHero.jsx
@@ -5,22 +5,25 @@ import img1 from "../../assets/landing-page-hero-img.png";
 
 import "../../styles/LandingPageHero.scss";
 
-const LandingPageHero = () => (
-  <div className="landing-page-hero-wrapper">
-    <p className="tools-title">money tools</p>
-    <p className="page-title">myMoney Calendar</p>
-    <p className="description">
-      Adjusting when you pay bills can help you save money, reduce strees, and
-      have more money available for what's important to you. This calendar will
-      help you visualize your spending and offer strategies to help you save.
-    </p>
-    <div className="lphero-img">
-      <img className="hero-image" src={img1} alt="calendar"></img>
+const LandingPageHero = () => {
+  return (
+    <div className="landing-page-hero-wrapper">
+      <p className="tools-title">money tools</p>
+      <p className="page-title">myMoney Calendar</p>
+      <p className="description">
+        Adjusting when you pay bills can help you save money, reduce strees, and
+        have more money available for what's important to you. This calendar
+        will help you visualize your spending and offer strategies to help you
+        save.
+      </p>
+      <div className="lphero-img">
+        <img className="hero-image" src={img1} alt="calendar"></img>
+      </div>
+      <Link to="/month">
+        <button className="lp-btn primary">Get started </button>
+      </Link>
     </div>
-    <Link to="/month">
-      <button className="lp-btn primary">Get started </button>
-    </Link>
-  </div>
-);
+  );
+};
 
 export default LandingPageHero;
