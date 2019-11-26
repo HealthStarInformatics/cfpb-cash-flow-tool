bbc3970bdad12bfa21a50473de5dc4e7991bad05
Missy Maloney <mmaloney@healthstarInfo.com>
2019-11-25 19:19:06 -0500

    formatting changes to LandingView for desktop

--- src/components/shared/HeadersFooters/SiteNavBar.jsx
+++ src/components/shared/HeadersFooters/SiteNavBar.jsx
@@ -11,68 +11,26 @@ import "../../../styles/SiteNavBar.scss";
 export const SiteNavBar = () => (
   <div className="sitenavbar-wrapper">
     <div className="component-body">
-      <div className="mobile">
-        <div className="image image-container-menu">
-          <img
-            className="image menu-icon"
-            src={menu}
-            alt="hamburger menu icon"
-          />
-        </div>
-        <div className="image image-container-logo">
-          <img
-            className="image cf-logo-small"
-            src={logo}
-            alt="CFPB small logo"
-          ></img>
-        </div>
-        <div className="image image-container-search">
-          <img
-            className="image search-icon"
-            src={search}
-            alt="search magnifying glass"
-          ></img>
-        </div>
+      <div className="image image-container-menu">
+        <img
+          className="image menu-icon"
+          src={menu}
+          alt="hamburger menu icon"
+        ></img>
       </div>
-      <div className="desktop">
-        <div className="desktop_top-row">
-          <div className="image image-container-logo header-left">
-            <img
-              className="image cf-logo-small"
-              src={logo}
-              alt="CFPB small logo"
-            ></img>
-          </div>
-          <div className="header-right">
-            <div className="image image-container-search">
-              <div>
-                <img
-                  className="image search-icon"
-                  src={search}
-                  alt="search magnifying glass"
-                ></img>
-              </div>
-              <div className="search-text">Search</div>
-            </div>
-            <div>
-              <Link>
-                {" "}
-                <span>icon</span>
-                <span>Submit a Complaint</span>
-              </Link>
-            </div>
-          </div>
-        </div>
-
-        <div className="desktop_bottom-row">
-          <ul className="menu-items">
-            <li className="">Consumer Tools</li>
-            <li>Practitioner Resources</li>
-            <li>Data & Research</li>
-            <li>Policy & Compliance</li>
-            <li>About Us</li>
-          </ul>
-        </div>
+      <div className="image image-container-logo">
+        <img
+          className="image cf-logo-small"
+          src={logo}
+          alt="CFPB small logo"
+        ></img>
+      </div>
+      <div className="image image-container-search">
+        <img
+          className="image search-icon"
+          src={search}
+          alt="search magnifying glass"
+        ></img>
       </div>
     </div>
   </div>
