e844409cceed6eae29e42c5e4eac9c486618ee34
Missy Maloney <mmaloney@healthstarInfo.com>
2019-12-16 17:42:08 -0500

    integrated design-system for LandingViewHero; added 4 sizes of dummy images for temporary use"

--- src/components/LandingView/LandingViewHero.jsx
+++ src/components/LandingView/LandingViewHero.jsx
@@ -1,29 +1,26 @@
 import React from "react";
 
-import img1 from "../../assets/landing-page-hero-img.png";
+// import img1 from "../../assets/landing-page-hero-img.png";
 
-// import "../../css/LandingViewHero.css";
+import "../../css/LandingViewHero.css";
 
 export const LandingViewHero = () => {
   return (
-    <div className="landing-view-hero-wrapper">
-      <div className="component-body">
-        <div className="hero_heading-wrapper">
-          <h1 className="hero-heading">myMoney Calendar</h1>
-          <p className="hero_subhead">
+    <section class="m-hero" id="hero1">
+      <div class="m-hero_wrapper wrapper">
+        <div class="m-hero_text">
+          <h1 class="m-hero_heading">myMoney Calendar</h1>
+          <p class="m-hero_subhead">
             Adjusting when you pay bills can help you save money, reduce strees,
             and have more money available for what's important to you. This
             calendar will help you visualize your spending and offer strategies
             to help you save.
           </p>
         </div>
-
-        <div className="hero_image-wrapper">
-          <div className="hero-image">
-            <img src={img1} alt="calendar"></img>
-          </div>
+        <div class="m-hero_image-wrapper">
+          <div class="m-hero_image"></div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
---
+++ src/components/LandingView/LandingViewHeroOLD.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import img1 from "../../assets/landing-page-hero-img.png";
+
+// import "../../css/LandingViewHero.css";
+
+export const LandingViewHero = () => {
+  return (
+    <div className="landing-view-hero-wrapper">
+      <div className="component-body">
+        <div className="hero_heading-wrapper">
+          <h1 className="hero-heading">myMoney Calendar</h1>
+          <p className="hero_subhead">
+            Adjusting when you pay bills can help you save money, reduce strees,
+            and have more money available for what's important to you. This
+            calendar will help you visualize your spending and offer strategies
+            to help you save.
+          </p>
+        </div>
+
+        <div className="hero_image-wrapper">
+          <div className="hero-image">
+            <img src={img1} alt="calendar"></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
